f5db8c25693cd78746e51ab5654f39f431d1625b
july22lop <88002008+july22lop@users.noreply.github.com>
2021-08-30 19:01:51 -0600

    negocios JL

--- Lab_Segundaparte/Lab_Segundaparte/Negocios/ClsPersona.cs
+++ Lab_Segundaparte/Lab_Segundaparte/Negocios/ClsPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab_Segundaparte.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,33 @@ namespace Lab_Segundaparte.Negocios
 {
     class ClsPersona
     {
-    }
+        Persona per = new Persona (0,0);
+        public String calcularIMC()
+        {
+
+            double pro = per.Peso / Math.Pow(per.Altura, 2);
+
+            if (pro < 20)
+            {
+                return "Su peso es ideal";
+            }
+            if (pro > 25)
+            {
+                return "Tiene sobre peso";
+            }
+            return "";
+        }
+        public String esMayorDeEdad()
+        {
+            if (per.Edad >= 18)
+            {
+                return "Es Mayor de edad";
+            }
+            else
+                return "No es mayor de edad";
+
+        }
+
+    }   
+        
 }
